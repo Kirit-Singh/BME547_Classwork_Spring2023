58b0cd8ddf7871f67462ab5451a8c55c89b1c073
Kirit Singh <kirit.singh@duke.edu>
2023-02-03 12:41:05 -0500

    Assed test_blood_calculator.py

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -8,3 +8,21 @@ def test_HDL_analysis():
     answer = HDL_analysis(HDL_input)
     # Assert
     assert answer == "Normal"
+
+def test_HDL_analysis_Borderline_Low():
+	from blood_calculator import test_HDL_analysis
+	# Arrange
+    HDL_input = 45
+    # Act
+    answer = HDL_analysis(HDL_input)
+    # Assert
+    assert answer == "Borderline Low"
+
+def test_HDL_analysis_Low():
+	from blood_calculator import test_HDL_analysis
+	# Arrange
+    HDL_input = 45
+    # Act
+    answer = HDL_analysis(HDL_input)
+    # Assert
+    assert answer == "Low"
