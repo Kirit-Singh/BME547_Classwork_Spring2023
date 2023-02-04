2a77304f2a94664f2009d4c6235bd083a7a3af85
Kirit Singh <kirit.singh@duke.edu>
2023-02-03 22:51:30 -0500

    New venv with fixed pytest and test function

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -1,30 +1,8 @@
-import pytest
-
-# 3A's - Arrange, Act, Assert
-@pytest.mark.parametrize("HDL_input, expected")
-def test_HDL_analysis(HDL_input, expected):
-	from blood_calculator import test_HDL_analysis
-	# Arrange
+def test_HDL_analysis():
+    from Blood_Calculator import HDL_analysis
+    # Arrange
     HDL_input = 65
     # Act
     answer = HDL_analysis(HDL_input)
     # Assert
     assert answer == "Normal"
-
-def test_HDL_analysis_Borderline_Low():
-	from blood_calculator import test_HDL_analysis
-	# Arrange
-    HDL_input = 45
-    # Act
-    answer = HDL_analysis(HDL_input)
-    # Assert
-    assert answer == "Borderline Low"
-
-def test_HDL_analysis_Low():
-	from blood_calculator import test_HDL_analysis
-	# Arrange
-    HDL_input = 45
-    # Act
-    answer = HDL_analysis(HDL_input)
-    # Assert
-    assert answer == "Low"
