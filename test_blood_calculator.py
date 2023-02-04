3a234abf5185fad0972b37950108eb92c51ba191
Kirit Singh <kirit.singh@duke.edu>
2023-02-03 22:54:42 -0500

    Added borderline low and low to test file

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -6,3 +6,21 @@ def test_HDL_analysis():
     answer = HDL_analysis(HDL_input)
     # Assert
     assert answer == "Normal"
+
+def test_HDL_analysis_Borderline_Low():
+    from Blood_Calculator import HDL_analysis
+    # Arrange
+    HDL_input = 50
+    # Act
+    answer = HDL_analysis(HDL_input)
+    # Assert
+    assert answer == "Borderline Low"
+
+def test_HDL_analysis_Low():
+    from Blood_Calculator import HDL_analysis
+    # Arrange
+    HDL_input = 30
+    # Act
+    answer = HDL_analysis(HDL_input)
+    # Assert
+    assert answer == "Low"
