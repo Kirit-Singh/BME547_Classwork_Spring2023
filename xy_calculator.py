0cb101a6f5843122627fdd211c181c4e67675a1c
Kirit Singh <kirit.singh@duke.edu>
2023-02-04 22:43:17 -0500

    created xy_calculator function and included slope calculator

---
+++ xy_calculator.py
@@ -0,0 +1,16 @@
+def xy_input():
+    x1 = float(input("Enter the value of x1: "))
+    y1 = float(input("Enter the value of y1: "))
+    x2 = float(input("Enter the value of x2: "))
+    y2 = float(input("Enter the value of y2: "))
+    first_coordinates = x1, y1
+    second_coordinates = x2, y2
+    return first_coordinates, second_coordinates
+
+def slope_calculator(first_coordinates, second_coordinates):
+    x1, y1 = first_coordinates
+    x2, y2 = second_coordinates
+    slope = (y2-y1)/(x2-x1)
+    return slope
+
+
