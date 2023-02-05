4dbaa562d7c3e2c2b5562d102b8a47fa2a10d689
Kirit Singh <kirit.singh@duke.edu>
2023-02-04 22:02:05 -0500

    Created test file for xy_calculator and set up test conditions

---
+++ test_xy_calculator.py
@@ -0,0 +1,7 @@
+test_def_xy_analysis
+    from xy_calculator import xy_analysis
+    first_coordinates = (1,1)
+    second_coordinates = (2,2)
+    answer = slope_calculator(first_coordinates,second_coordinates)
+    expected = 1
+    assert answer == expected
