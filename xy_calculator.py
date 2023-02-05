3ed87a1011e7fecc8d4d27b4eabb5fb1a62bdeee
Kirit Singh <kirit.singh@duke.edu>
2023-02-04 22:56:35 -0500

    Added xy_analysis code block and print function containing input for x3

--- xy_calculator.py
+++ xy_calculator.py
@@ -11,6 +11,14 @@ def slope_calculator(first_coordinates, second_coordinates):
     x1, y1 = first_coordinates
     x2, y2 = second_coordinates
     slope = (y2-y1)/(x2-x1)
-    return slope
+    return x1, y1, slope
 
+def xy_analysis(x1, y1, slope, x3):
+	y3 = (slope * (x3 - x1)) + y1
+	return y3
 
+first_coordinates, second_coordinates = xy_input()
+x1, y1, slope = slope_calculator(first_coordinates, second_coordinates)
+x3 = float(input("Enter the value of x3: "))
+result = xy_analysis(x1, y1, slope, x3)
+print("The value of y3 is:", result)
