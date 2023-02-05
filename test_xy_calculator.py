0a7b6094643cdc3ac9a4931970d4cef57f1f1069
Kirit Singh <kirit.singh@duke.edu>
2023-02-04 23:13:42 -0500

    split test functions out for slope calculator and xy analysis

--- test_xy_calculator.py
+++ test_xy_calculator.py
@@ -1,7 +1,17 @@
-def test_xy_analysis():
-    from xy_calculator import xy_analysis
-    first_coordinates = (1,1)
-    second_coordinates = (2,2)
-    answer = slope_calculator(first_coordinates,second_coordinates)
-    expected = 1
+def test_slope_calculator():
+    from xy_calculator import slope_calculator
+    first_coordinates = (4, 4)
+    second_coordinates = (8, 8)  
+    answer = slope_calculator(first_coordinates, second_coordinates)
+    expected = (4, 4, 1)
     assert answer == expected
+
+def test_xy_analysis():
+	from xy_calculator import xy_analysis
+	x1 = 2
+	y1 = 2
+	slope = 1
+	x3 = 6
+	answer = xy_analysis(x1, y1, slope, x3)
+	expected = 6
+	assert answer == expected
