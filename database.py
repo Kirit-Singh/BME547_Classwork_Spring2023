45fd01799af236ac6c231af58ab6b34e3711f610
Kirit Singh <kirit.singh@duke.edu>
2023-02-08 13:15:53 -0500

    fixed PEP8 style for database.py

--- database.py
+++ database.py
@@ -1,13 +1,18 @@
+import Blood_Calculator as bc
+
+
 print("This is the database.py file")
 print("Python thinks this is called {}".format(__name__))
 
-import Blood_Calculator as bc
 
-HDL=55
+HDL = 55
+
 
 HDL_analysis = bc.HDL_analysis(HDL)
 
+
 print("The HDL analysis is {}".format(HDL_analysis))
+
 
 bc.generic_input("Other Test")
 print(bc.LDL_analysis(13))
