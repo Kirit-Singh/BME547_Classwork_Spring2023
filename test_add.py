331011700972a22245d6a6118a95fa08eb7508a8
Kirit Singh <kirit.singh@duke.edu>
2023-02-08 13:21:51 -0500

    fixed PEP8 style in test_add.py

--- test_add.py
+++ test_add.py
@@ -2,6 +2,6 @@ import pytest
 
 
 def test_add():
-	from add import addition
-	answer = addition (0.1, 0.2)
-	print(pytest.approx(0.3))
+    from add import addition
+    answer = addition(0.1, 0.2)
+    print(pytest.approx(0.3))
