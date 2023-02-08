ac68256e3178c5fad88a80065ea97ba46ed2707d
Kirit Singh <kirit.singh@duke.edu>
2023-02-08 13:22:44 -0500

    Fixed PEP8 formatting in test_check_fever.py

--- test_check_fever.py
+++ test_check_fever.py
@@ -1,10 +1,6 @@
-def test_check_fever(): 
+def test_check_fever():
     from Blood_Calculator import check_fever
     input_temps = [97, 98.6, 100.1, 103, 98.4]
     answer = check_fever(input_temps)
     expected = True
     assert answer == expected
-
-
-
-	
