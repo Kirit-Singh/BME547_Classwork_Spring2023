a68a3cf675c108b043bf1614ed5ba27cd568b35f
Kirit Singh <kirit.singh@duke.edu>
2023-02-10 13:14:45 -0500

    added age test to classes file

--- database_class.py
+++ database_class.py
@@ -1,10 +1,13 @@
 class Patient:
 
-    def __init__(self):
-        self.first_name = ""
-        self.last_name = ""
-        self.mrn = 0
-        self.age = 0
+    def __init__(self, patient_first_name,
+                 patient_last_name,
+                 patient_mrn,
+                 patient_age):
+        self.first_name = patient_first_name
+        self.last_name = patient_last_name
+        self.mrn = patient_mrn
+        self.age = patient_age
         self.tests = []
 
     def get_full_name(self):
@@ -15,20 +18,27 @@ class Patient:
         print(self.age)
         return full_name
 
+    def is_adult(self):
+        if self.age >= 18:
+            return True
+        else:
+            return False
+
 
 def main():
-    new_patient = Patient()
-    second_patient = Patient()
+    new_patient = Patient("Ann", "Ables", 1, 34)
+    second_patient = Patient("Bob", "Boyles", 2, 45)
     print(new_patient)
     print(second_patient)
-    new_patient.first_name = "Kirit"
-    new_patient.last_name = "Singh"
+    # new_patient.first_name = "Kirit"
+    # new_patient.last_name = "Singh"
     new_patient.tests.append(("HDL", 100))
     print(new_patient.first_name)
     print(new_patient.last_name)
     print(new_patient.tests)
     print(new_patient.get_full_name())
     print(second_patient.first_name)
+    print(new_patient.is_adult())
 
 
 if __name__ == "__main__":
