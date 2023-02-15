fd2f27754ee50400d722445ddff96786c96887b9
Kirit Singh <kirit.singh@duke.edu>
2023-02-15 12:31:54 -0500

    Updated with new ways to read lines from sample files

--- input_fruit.py
+++ input_fruit.py
@@ -1,1 +1,13 @@
 in_file = open("input_data.txt", "r")
+fruits = in_file.readlines()
+print(fruits)
+in_file.close()
+
+
+in_file = open("input_data.txt", "r")
+for line in in_file:
+    print(line)
+
+
+first_fruit = in_file.readline()
+second_fruit = in_file.readline()
