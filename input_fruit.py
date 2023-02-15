2c9270a20dd71d592275d7817da0ba85619a7f56
Kirit Singh <kirit.singh@duke.edu>
2023-02-15 12:40:09 -0500

    updated to add test functions and different ways of splitting input

--- input_fruit.py
+++ input_fruit.py
@@ -1,13 +1,18 @@
-in_file = open("input_data.txt", "r")
-fruits = in_file.readlines()
-print(fruits)
-in_file.close()
+def analyze_ID(input_line):
+    patient_data = first_line.strip("/n").split("=")
+    patient_id = int(patient_data[1])
+    return patient_id
 
 
-in_file = open("input_data.txt", "r")
-for line in in_file:
-    print(line)
+def read_file():
+    in_file = open(filename, "r")
+    first_line = in_file.readline()
+    id = analyze_ID(first_line)
 
 
-first_fruit = in_file.readline()
-second_fruit = in_file.readline()
+def test_read_file():
+    from module import read_file
+    filename = "my_test_data.txt"
+    answer = read_file(filename)
+    expected = 50
+    assert == expected
