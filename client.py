dd9df5af70e4d79994350259cf385f59cab0f034
Kirit Singh <kirit.singh@duke.edu>
2023-03-03 12:46:09 -0500

    Updated add function and jsonify

--- client.py
+++ client.py
@@ -1,9 +1,23 @@
 import requests
 
 
-server ="http://127.0.0.1:5000"
+server = "http://127.0.0.1:5000"
 
 
-r=requests.get(server + "/info")
+r = requests.get(server + "/info")
 print(r.status_code)
 print(r.text)
+
+
+out_data = {"name": "Kirit", "HDL_value": 55}
+r = requests.post(server + "/HDL_analysis", json=out_data)
+print(r.status_code)
+print(r.text)
+
+
+out_data = {"a": 2, "b": 3}
+r = requests.post(server + "/add", json=out_data)
+print(r.status_code)
+print(r.text)
+x = r.json()
+print(x + 2)
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
@@ -12,6 +12,26 @@ def server_status():
 @app.route("/info", methods=["GET"])
 def info_route():
     return "This server was written for BME 547"
+
+
+@app.route("/HDL_analysis", methods=["POST"])
+def HDL_route_handler():
+    """
+    in_data = {"name": <patient_name>,
+               "HDL_value": <HDL_result> }
+    """
+    from Blood_Calculator import HDL_analysis
+    in_data = request.get_json()
+    print("Received HDL value of {}".format(in_data["HDL_value"]))
+    diagnosis = HDL_analysis(in_data["HDL_value"])
+    return diagnosis
+
+
+@app.route("/add", methods=["POST"])
+def add_numbers():
+    in_data = request.get_json()
+    answer = in_data["a"] + in_data["b"]
+    return jsonify(answer)
 
 
 if __name__ == "__main__":
