6aeedb8067a34cedcaab9837eb527c51619730d5
Kirit Singh <kirit.singh@duke.edu>
2023-03-02 22:16:48 -0500

    updated with way to pull out data in json format

--- github_client.py
+++ github_client.py
@@ -1,6 +1,13 @@
 import requests
 
 
-r = requests.get("https://api.github.com/repos/Kirit-Singh/BME547_CLasswork_Spring2023/branches")
+r = requests.get("https://api.github.com/repos/"
+                 "Kirit-Singh/BME547_CLasswork_Spring2023/branches")
 print(r)
 print(type(r))
+print(r.status_code)
+print(r.text)
+branches = r.json()
+print(branches)
+for branch in branches:
+    print(branch["name"])
