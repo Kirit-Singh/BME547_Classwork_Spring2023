5971fc4f41fcb314c99db616d70d29627a0e92dd
Kirit Singh <kirit.singh@duke.edu>
2023-03-02 22:52:41 -0500

    changed to add server variable

--- blood_match.py
+++ blood_match.py
@@ -1,23 +1,21 @@
 import requests
 
 
-patient_IDs = requests.get("http://vcm-7631.vm.duke.edu:"
-                           "5002/get_patients/ks625")
+server = "http://vcm-7631.vm.duke.edu:5002"
+
+patient_IDs = requests.get(server + "/get_patients/ks625")
 print(patient_IDs.text)
 
 
-p1_type = requests.get("http://vcm-7631.vm.duke.edu:"
-                       "5002/get_blood_type/F8")
+p1_type = requests.get(server + "/get_blood_type/F8")
 print(p1_type.text)
 
 
-p2_type = requests.get("http://vcm-7631.vm.duke.edu:"
-                       "5002/get_blood_type/F3")
+p2_type = requests.get(server + "/get_blood_type/F3")
 print(p2_type.text)
 
 
 out_data = {"Name": "ks625", "Match": "No"}
-blood_match = requests.post("http://vcm-7631.vm.duke.edu:"
-                            "5002/match_check", json=out_data)
+blood_match = requests.post(server + "/match_check", json=out_data)
 print(blood_match.status_code)
 print(blood_match.text)
